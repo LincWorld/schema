c893674fc0b90f722b80beb997509910de727175
Afzaal <afzaal@linc.world>
2023-10-25 19:53:47 +0000

    Reinit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require("node:path");
 const fs = require("node:fs/promises");
 const Generator = require("@asyncapi/generator");
-const {recursiveDownload} = require('gh-retrieve');
-const generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, 'dist/asyncapi'), {
+const { recursiveDownload } = require('gh-retrieve');
+const generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, 'docs/asyncapi'), {
 
 	forceWrite: true,
 	templateParams: {
@@ -14,22 +14,28 @@ const generator = new Generator('@asyncapi/html-template', path.resolve(__dirnam
 
 
 const downloadSwagger = async () => {
-	await fetch("https://api.github.com/repos/swagger-api/swagger-ui/releases/latest").then((res)=>
-		res.json()
-	).then((res)=>res.tag_name).then((tag)=>{
-		return recursiveDownload({
-			author: "swagger-api", //repository owner
-			repo: "swagger-ui", //repository name
-			targetdir: "dist", //target directory to download
-			outdir: "dist/openapi", //directory to download in,
-			branch: tag
-		 })
-	}).then(()=>{
+	await fs.stat('./docs/openapi/index.html').then((stat) => {
+		if (stat.isFile) {
+			console.log("Swagger already downloaded");
+			return;
+		}
+		return fetch("https://api.github.com/repos/swagger-api/swagger-ui/releases/latest").then((res) =>
+			res.json()
+		).then((res) => res.tag_name).then((tag) => {
+			return recursiveDownload({
+				author: "swagger-api", //repository owner
+				repo: "swagger-ui", //repository name
+				targetdir: "dist", //target directory to download
+				outdir: "docs/openapi", //directory to download in,
+				branch: tag
+			})
+		})
+	}).then(() => {
 		console.log("Swagger Downloaded Successfully");
 		Promise.all([
-			fs.readFile("./dist/openapi/swagger-initializer.js", "utf8").then((content)=>{
-				content.replace("https://petstore.swagger.io/v2/swagger.json","https://schema.linc.world/openapi.json");
-				fs.writeFile("./dist/openapi/swagger-initializer.js", content);
+			fs.readFile("./docs/openapi/swagger-initializer.js", "utf8").then((content) => {
+				content = content.replace(/petstore\.swagger\.io\/v2\/swagger\.json/m, "schema.linc.world/openapi.json");
+				fs.writeFile("./docs/openapi/swagger-initializer.js", content);
 			}),
 		])
 	})
@@ -37,10 +43,9 @@ const downloadSwagger = async () => {
 const main = async () => {
 	await downloadSwagger();
 	await generator.generateFromFile('./schemas/asyncapi.json');
-	await fs.copyFile("./schemas/asyncapi.json","./dist/asyncapi.json");
-	await fs.copyFile("./schemas/openapi.json","./dist/openapi.json");
-	await fs.writeFile("./dist/index.html","");
+	await fs.copyFile("./schemas/asyncapi.json", "./docs/asyncapi.json");
+	await fs.copyFile("./schemas/openapi.json", "./docs/openapi.json");
+	await fs.writeFile("./docs/index.html", "");
 }
 
 main();
-// https://github.com/swagger-api/swagger-ui/tree/v5.9.1/dist
